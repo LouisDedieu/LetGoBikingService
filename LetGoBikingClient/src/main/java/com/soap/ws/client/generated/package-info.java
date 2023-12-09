23c8088e3a13469f052ef2ad503385c78bcfdfec
victor dedieu <louis.victordedieu@gmail.com>
2023-12-09 22:36:58 +0100

    Mise à jour des fichiers suivant les nouvelles règles de .gitignore

--- LetGoBikingClient/src/main/java/com/soap/ws/client/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.datacontract.org/2004/07/LetGoBikingService.Models", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.soap.ws.client.generated;
